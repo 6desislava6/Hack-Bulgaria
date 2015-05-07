877445b70a55af0fbddce9006cdb4b50dedd8a0b
Desislava Tsvetkova <desislavatsvetkova@mail.bg>
2015-05-07 12:45:01 +0300

    Changed commands, Capital letters

--- Hack-eight-week/Wednesday/sqlite3_commands.sql
+++ Hack-eight-week/Wednesday/sqlite3_commands.sql
@@ -1,31 +1,42 @@
-Select Firstname, LastName, Title FROM employees;
-Select * FROM employees where city = 'Seattle';
-Select * FROM employees where city = 'London';
-Select * FROM employees where Title LIKE '%Sales%';
-Select * FROM employees where Title LIKE '%Sales%' AND (TitleOfCourtesy = 'Ms.' OR TitleOfCourtesy = 'Mrs.') ;
-Select * FROM employees ORDER BY BirthDate LIMIT 5;
-Select * FROM employees ORDER BY HireDate LIMIT 5;
-Select * FROM employees where ReportsTo is null;
-Select a.FirstName, b.FirstName from employees as a JOIN employees as b on a.ReportsTo = b.EmployeeID;
-Select COUNT(*) FROM employees WHERE (TitleOfCourtesy = 'Ms.' OR TitleOfCourtesy = 'Mrs.') ;
-Select COUNT(*) FROM employees WHERE (TitleOfCourtesy = 'Mr.' OR TitleOfCourtesy = 'Dr.') ;
-SELECT * FROM (SELECT DISTINCT City From employees) as a JOIN (select FirstName, City from employees) as b ON a.City = b.City;
-SELECT * FROM (SELECT DISTINCT City From employees) as a JOIN (select FirstName, City from employees) as b ON a.City = b.City;
-Select count(*) from employees group by City;
-Select OrderID, FirstName, LastName From orders JOIN employees ON orders.EmployeeID = employees.EmployeeID; 
-Select OrderID, CompanyName From orders JOIN shippers ON orders.ShipVia = shippers.ShipperID; 
-Select count(*) from orders group by ShipCountry;
-Select count(*), EmployeeID from orders group by EmployeeID;
-Select FirstName, LastName from employees as a inner join orders as b on a.EmployeeID = b.EmployeeID 
-group by b.EmployeeID
-order by count(b.EmployeeID) desc limit 1; 
-Select CompanyName from customers as a inner join orders as b on a.CustomerID = b.CustomerID 
-group by b.CustomerID
-order by count(b.CustomerID) desc limit 1; 
-Select OrderID, FirstName, CompanyName FROM ((orders JOIN employees ON orders.EmployeeID = employees.EmployeeID) as a JOIN customers ON a.CustomerID = customers.CustomerID);
-SELECT a.OrderID, b.CompanyName, c.CompanyName
+SELECT Firstname, LastName, Title FROM employees;
+SELECT * FROM employees WHERE city = 'Seattle';
+SELECT * FROM employees WHERE city = 'LONdON';
+SELECT * FROM employees WHERE Title LIKE '%Sales%';
+SELECT * FROM ewhmployees WHERE Title LIKE '%Sales%' AND (TitleOfCourtesy = 'Ms.' OR TitleOfCourtesy = 'Mrs.') ;
+SELECT * FROM employees ORDER BY BirthDate LIMIT 5;
+SELECT * FROM employees ORDER BY HireDate LIMIT 5;
+SELECT * FROM employees WHERE ReportsTo is null;
+SELECT a.FirstName, b.FirstName
+FROM employees AS a JOIN employees AS b ON a.ReportsTo = b.EmployeeID;
+SELECT COUNT(*) FROM employees
+WHERE (TitleOfCourtesy = 'Ms.' OR TitleOfCourtesy = 'Mrs.') ;
+SELECT COUNT(*) FROM employees
+ WHERE (TitleOfCourtesy = 'Mr.' OR TitleOfCourtesy = 'Dr.') ;
+SELECT * FROM (SELECT DISTINCT City FROM employees)
+AS a JOIN (SELECT FirstName, City FROM employees) AS b ON a.City = b.City;
+SELECT * FROM (SELECT DISTINCT City FROM employees)
+AS a JOIN (SELECT FirstName, City FROM employees) AS b ON a.City = b.City;
+SELECT count(*) FROM employees GROUP BY City;
+SELECT ORDERID, FirstName, LastName FROM orders JOIN
+employees ON orders.EmployeeID = employees.EmployeeID;
+SELECT ORDERID, CompanyName FROM orders JOIN shippers ON orders.ShipVia = shippers.ShipperID;
+SELECT count(*) FROM orders GROUP BY ShipCountry;
+SELECT count(*), EmployeeID FROM orders
+ GROUP BY EmployeeID;
+SELECT FirstName, LastName FROM employees AS a inner JOIN
+ orders AS b ON a.EmployeeID = b.EmployeeID
+GROUP BY b.EmployeeID
+ORDER BY count(b.EmployeeID) DESC LIMIT 1;
+SELECT CompanyName FROM customers AS a inner JOIN orders
+ AS b ON a.CustomerID = b.CustomerID
+GROUP BY b.CustomerID
+ORDER BY count(b.CustomerID) DESC LIMIT 1;
+SELECT ORDERID, FirstName, CompanyName FROM
+((orders JOIN employees ON orders.EmployeeID = employees.EmployeeID)
+ AS a JOIN customers ON a.CustomerID = customers.CustomerID);
+SELECT a.ORDERID, b.CompanyName, c.CompanyName
 FROM orders AS a
-JOIN customers as b
-on a.CustomerID = b.CustomerID
-JOIN shippers as c
+JOIN customers AS b
+ON a.CustomerID = b.CustomerID
+JOIN shippers AS c
 ON a.ShipVia = c.ShipperID;
